55354380b20ce5c77320596ae3e643ac0e13e3a8
ThanhBinh101 <nguyenthithanhbinh1012004@gmail.com>
2024-01-27 10:24:12 +0700

    Nahhhhhh
    here we go again

---
+++ Assets/CharacterMovement.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+using System;
+
+public class CharacterMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movementInput;
+    private bool isJumping;
+    private bool isGrounded;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float speed = 5f;
+    public float jumpForce = 10f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+        CheckGrounded();
+    }
+
+    void CheckGrounded()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.7f, groundLayer);
+        Debug.Log(isGrounded);
+    }
+
+    void Move()
+    {
+        Vector2 moveDirection = new Vector2(movementInput.x, 0);
+        rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y);
+
+        if (isJumping)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+            isJumping = false;
+        }
+
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(moveDirection.x * speed, rb.velocity.y-2f);
+        }
+        
+
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        movementInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            isJumping = true;
+        }
+    }
+}
