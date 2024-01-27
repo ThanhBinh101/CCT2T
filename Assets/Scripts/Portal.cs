6122c12b2159aa2147ccd4d2ce6d3895dd81725d
ThanhBinh101 <nguyenthithanhbinh1012004@gmail.com>
2024-01-27 12:01:41 +0700

    demoportal
    first portal

--- Assets/Scripts/Portal.cs
+++ Assets/Scripts/Portal.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class Portal : MonoBehaviour
 {
-    
+    private Transform destination;
+    public bool isBlue;
+    public float distance = 0.2f;
+
+    private void Start()
+    {
+        if (isBlue)
+        {
+            destination = GameObject.FindGameObjectWithTag("GreenPortal").GetComponent<Transform>();
+
+        }
+        else
+        {
+            destination = GameObject.FindGameObjectWithTag("BluePortal").GetComponent<Transform>();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (Vector2.Distance(transform.position, other.transform.position) > distance)
+        {
+            other.transform.position = new Vector2(destination.position.x, destination.position.y);
+
+        }
+    }
+
+
 }
