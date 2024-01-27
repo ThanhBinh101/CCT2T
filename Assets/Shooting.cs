147ff9b158e6ccbd7f48cb17be1c5d8636e9af3d
Kirigaya0309 <muondang1234@gmail.com>
2024-01-27 15:35:14 +0700

    add pushing

--- Assets/Shooting.cs
+++ Assets/Shooting.cs
@@ -10,12 +10,20 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
     public float speed = 3;
 
+    public Transform pushingPoint;
+    public GameObject pushPref;
+
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.R))
         {
             var bullet = Instantiate(bulletPrefab, shootingPoint.position, transform.rotation);
             bullet.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            var push = Instantiate(pushPref, pushingPoint.position, transform.rotation);
+            push.GetComponent<Rigidbody2D>().velocity = transform.right * speed;
         }
     }
 }
