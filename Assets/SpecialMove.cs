e3fca0403edddbe6c5528434f272ea4eced536a1
ThanhBinh101 <nguyenthithanhbinh1012004@gmail.com>
2024-01-27 13:01:19 +0700

    Add some global variable
    character, public funtion

--- Assets/SpecialMove.cs
+++ Assets/SpecialMove.cs
@@ -9,31 +9,34 @@ public class SpecialMove : MonoBehaviour
     public int count3;
     public int count4;
 
+    [SerializeField] public GameObject player;
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         count1 = count2 = count3 = count4 = 0;
+
     }
 
     // Update is called once per frame
-    void Update()
+    public void Update()
     {
         
     }
 
-    void usePortal() {
-        if (count2 > 0) {
-            //portal ability here
-        } else {
-            Debug.Log ("Out of stock!");
-        }
+    void useGun()
+    {
+
+    }
+    
+
+    public void usePortal() {
+        if (count2 == 0) return;
+        --count2;
+       
     }
 
-    void useDoubleJump() {
-        if (count1 > 0) {
-
-        } else {
-            Debug.Log ("Out of stock!");
-        }
+    public void useDoubleJump() {
+        
     }
 }
