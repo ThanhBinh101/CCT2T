3f19e1e6fe4e279b3e3c5b2ad309d26bf800671c
ThanhBinh101 <nguyenthithanhbinh1012004@gmail.com>
2024-01-28 17:27:16 +0700

    UI Complete

--- Assets/Scripts/SkillCount.cs
+++ Assets/Scripts/SkillCount.cs
@@ -7,29 +7,24 @@ using UnityEngine.UI;
 public class SkillCount : MonoBehaviour
 {
     [SerializeField] public TextMeshProUGUI textMP;
-    [SerializeField] public GameObject image;
     [SerializeField] public SpecialMove character;
 
     private void FixedUpdate()
     {
         if (gameObject.tag == "Initial")
         {
-            image.SetActive(character.count1 == 0);
             textMP.text = character.count1.ToString();
         }
         if (gameObject.tag == "JumpPush")
         {
-            image.SetActive(character.count2 == 0);
             textMP.text = character.count2.ToString();
         }
         if (gameObject.tag == "Portal")
         {
-            image.SetActive(character.count3 == 0);
             textMP.text = character.count3.ToString();
         }
         if (gameObject.tag == "Swap")
         {
-            image.SetActive(character.count4 == 0);
             textMP.text = character.count4.ToString();
         }
     }
